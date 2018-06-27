edb3aba0cb4b2a1bbc6fdcc5b65011e24dfc2acf
mark <mark.foster@abracontrols.com>
2018-06-27 10:06:13 -0600

    restore defaults in reset of store

--- src/reducers/lasfile.js
+++ src/reducers/lasfile.js
@@ -33,16 +33,15 @@ const lasFile = (state = initialState, action) => {
         chunk: state.chunk + 1
       }
       case RESET_LAS://reset ascii only
-      return {}
-      // return {
-      //   ...state,
-      //   [ASCII]: {
-      //     data:[]
-      //   },
-      //   ['CURVE_INFORMATION']:{
+      return {
+        ...state,
+        [ASCII]: {
+          data:[]
+        },
+        ['CURVE_INFORMATION']:{
           
-      //   }
-      // }
+        }
+      }
     case ADD_SECTION:
       return {
         ...state,
