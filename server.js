782c773438f14198a40df4a16399b4cb7a303715
Mark Foster <mark.foster@abracontrols.com>
2018-10-23 18:46:39 -0600

    new version

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const app = express()
 import path  from 'path'
 app.use(express.static('dist'))
 app.get('/*', (req, res) =>{
-  res.sendFile(path.join(__dirname, '/dist/index.html'), (err) =>{
+  res.sendFile(path.resolve(__dirname+ '/../dist/index.html'), function (err) {
+  //res.sendFile(path.join(__dirname, '/dist/index.html'), (err) =>{
     if (err)
       res.status(500).send(err)
   })
